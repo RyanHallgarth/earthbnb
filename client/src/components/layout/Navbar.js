2c5600d47d8bad58cd45b71ccf444cc9854ff627
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-04-10 21:12:57 -0400

    Setting up react router boilerplate

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>EarthBnB</h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Just boilerplate while setting up react router</p>
+    </div>
+  );
+};
+
+export default About;
