483987c62f40172a95f48f7b693d18e2e2f6ea83
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-04-14 15:46:49 -0400

    Pagination implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,44 @@ import { Fragment, useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Pagination from "./components/layout/Pagination";
 import About from "./components/pages/About";
+import Search from "./components/locations/Search";
 import Locations from "./components/locations/Locations";
 import axios from "axios";
 
 function App() {
   const [locations, setLocations] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [payload, setPayload] = useState({});
 
-  // const listLocations = async () => {
+  // useEffect(() => {
   //   console.log("yee");
-  //   const res = await axios.get(
-  //     "v1/listings?page=1&limit=10&sort_by=title&order=asc"
-  //   );
-  // };
+  //   axios
+  //     .get("v1/listings?page=5&limit=10&sort_by=title&order=asc")
+  //     .then((res) => setLocations(res.data.content));
+  // }, []);
 
-  useEffect(() => {
-    console.log("yee");
-    const res = axios
-      .get("v1/listings?page=1&limit=10&sort_by=title&order=asc")
-      .then((res) => setLocations(res.data.content));
-  }, []);
+  const searchLocations = async (text) => {
+    const res = await axios.get(
+      "v1/listings?page=1&limit=100&sort_by=title&order=asc"
+    );
+
+    setLocations(res.data.content);
+    setPayload(res.data);
+  };
+
+  //Get number of locations
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = locations.slice(indexOfFirstPost, indexOfLastPost);
+
+  //Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <Router>
@@ -29,9 +47,18 @@ function App() {
         <Navbar />
         <div className='container'>
           <Switch>
+            <Fragment>
+              <Search searchLocations={searchLocations} />
+              <Locations locations={currentPosts} />
+              <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={locations.length}
+                paginate={paginate}
+              />
+            </Fragment>
+
             <Route exact path='/about' component={About} />
           </Switch>
-          <Locations locations={locations} />
         </div>
       </Fragment>
     </Router>
