09567d751b218dbd1a19121523cd710218ee5f4b
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-04-17 18:47:13 -0400

    removed Navbar.js & About.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pagination from "./components/layout/Pagination";
-import About from "./components/pages/About";
 import Home from "./components/pages/Home";
 import Header from "./components/layout/Header";
-//import Search from "./components/locations/Search";
 import Locations from "./components/locations/Locations";
 import Location from "./components/locations/Location";
 import axios from "axios";
@@ -14,16 +12,12 @@ function App() {
   const [locations, setLocations] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
-  const [payload, setPayload] = useState({});
 
   const searchLocations = async (text) => {
-    const res = await axios.get(
-      "v1/listings?page=1&limit=100&sort_by=title&order=asc"
-    );
+    const res = await axios.get("/api/v1/listings?page=1&limit=5");
 
     setLocations(res.data.content);
     console.log(locations);
-    setPayload(res.data);
   };
 
   //Get number of locations
@@ -46,7 +40,6 @@ function App() {
           <Switch>
             <Route exact path='/'>
               <Fragment>
-                {/*<Search searchLocations={searchLocations} />*/}
                 <Locations locations={currentPosts} />
                 <Pagination
                   postsPerPage={postsPerPage}
@@ -55,8 +48,6 @@ function App() {
                 />
               </Fragment>
             </Route>
-
-            <Route exact path='/about' component={About} />
             <Route
               exact
               path='/location/:id'
