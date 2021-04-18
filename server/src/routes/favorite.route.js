29df94772c215a16926201f1017093f6fc70099c
Adam Clarkson <atclarks@iu.edu>
2021-04-18 14:20:06 -0400

    commented out a line to make code work

--- server/src/routes/favorite.route.js
+++ server/src/routes/favorite.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const listingRouter = require('./listing.route');
 const favoriteRouter = express.Router();
 
-listingRouter.get('/', paginatedResults(), (req, res) => {});
+//listingRouter.get('/', paginatedResults(), (req, res) => {});
 
 listingRouter.post('/login?:id', (req, res) => {
   console.log('work in progress');
