6210da0de3c1d313fe8a562ed06b94f46c9b0107
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-04-21 14:28:29 -0400

    various CSS tweaks, side-scroll totally gone I think. done with details page (for now)

--- client/src/components/locations/Location.js
+++ client/src/components/locations/Location.js
@@ -2,13 +2,14 @@ import { React, useEffect } from "react";
 import { Link } from "react-router-dom";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
 import HotelIcon from "@material-ui/icons/Hotel";
 import HouseIcon from "@material-ui/icons/House";
 import BathtubIcon from "@material-ui/icons/Bathtub";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
-import BookIcon from "@material-ui/icons/Book";
+import StarIcon from "@material-ui/icons/Star";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import { Button } from "@material-ui/core";
 import "../../Location.css";
 import Map from "../Map";
 
@@ -22,18 +23,14 @@ const Location = ({ location, match, getLocation }) => {
     description,
     picture_url,
     street,
-    neighborhood,
+    neighbourhood,
     room_type,
     accommodates,
     bathrooms,
     bedrooms,
     beds,
-    amenities,
-    guests_included,
     has_availabilitruey,
     review_scores_rating,
-    number_of_reviews,
-    cancellation_policy,
     hostrue_is_superhostrue,
     host_name,
     property_type,
@@ -41,13 +38,20 @@ const Location = ({ location, match, getLocation }) => {
     notes,
     latitude,
     longitude,
+    price,
+    weekly_price,
   } = location;
 
   return (
     <div className='card'>
-      <Link to='/search' className='btn btn-light'>
+      <Button
+        to={`/search`}
+        component={Link}
+        variant='outlined'
+        className='more-btn'
+      >
         Back to Search
-      </Link>
+      </Button>
       <div className='card grid-1'>
         <div className='title'>
           <h1>{name}</h1>
@@ -62,7 +66,10 @@ const Location = ({ location, match, getLocation }) => {
                 style={{ color: "green", fontSize: "20px" }}
               />
             ) : (
-              <HighlightOffIcon className='icon' style={{ color: "red" }} />
+              <HighlightOffIcon
+                className='icon'
+                style={{ color: "red", fontSize: "20px" }}
+              />
             )}
           </div>
           <div className='icon-text'>
@@ -73,7 +80,10 @@ const Location = ({ location, match, getLocation }) => {
                 style={{ color: "green", fontSize: "20px" }}
               />
             ) : (
-              <HighlightOffIcon className='icon' style={{ color: "red" }} />
+              <HighlightOffIcon
+                className='icon'
+                style={{ color: "red", fontSize: "20px" }}
+              />
             )}
           </div>
           <div className='icon-text'>
@@ -84,7 +94,10 @@ const Location = ({ location, match, getLocation }) => {
                 style={{ color: "green", fontSize: "20px" }}
               />
             ) : (
-              <HighlightOffIcon className='icon' style={{ color: "red" }} />
+              <HighlightOffIcon
+                className='icon'
+                style={{ color: "red", fontSize: "20px" }}
+              />
             )}
           </div>
         </div>
@@ -98,6 +111,7 @@ const Location = ({ location, match, getLocation }) => {
         <h2>
           {property_type} hosted by {host_name}
         </h2>
+        <h4 className='address'>in the {neighbourhood} neighborhood</h4>
         <h4 className='address'>{street}</h4>
       </div>
       <div className='card grid-2'>
@@ -118,6 +132,19 @@ const Location = ({ location, match, getLocation }) => {
               </li>
               <p className='note'>You'll have the {room_type} to yourself</p>
               <li className='flex-container'>
+                <span>
+                  <AttachMoneyIcon
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "30px",
+                      color: "#474747",
+                    }}
+                  />
+                </span>
+                Costs ${price} per night
+              </li>
+              <p className='note'>Or a weekly price of ${weekly_price}</p>
+              <li className='flex-container'>
                 {" "}
                 <span>
                   <PeopleAltIcon
@@ -136,49 +163,73 @@ const Location = ({ location, match, getLocation }) => {
             </ul>
           </div>
         </div>
-        <div className='top-deets'>
-          <ul>
-            <li class='flex-container'>
-              {" "}
-              <span>
-                <MeetingRoomIcon
-                  style={{
-                    marginRight: "15px",
-                    fontSize: "30px",
-                    color: "#474747",
-                  }}
-                />
-              </span>
-              {bedrooms} Bedroom(s)
-            </li>
-            <li class='flex-container'>
-              {" "}
-              <span>
-                <HotelIcon
-                  style={{
-                    marginRight: "15px",
-                    fontSize: "30px",
-                    color: "#474747",
-                  }}
-                />
-              </span>
-              {beds} Bed(s)
-            </li>
-            <li class='flex-container'>
-              {" "}
-              <span>
-                <BathtubIcon
-                  style={{
-                    marginRight: "15px",
-                    fontSize: "30px",
-                    color: "#474747",
-                  }}
-                />
-              </span>
-              {bathrooms} Bathroom(s)
-            </li>
-          </ul>
+        <div className='mb'>
+          <div className='top-deets'>
+            <ul>
+              <li className='flex-container'>
+                {" "}
+                <span>
+                  <MeetingRoomIcon
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "30px",
+                      color: "#474747",
+                    }}
+                  />
+                </span>
+                {bedrooms} Bedroom(s)
+              </li>
+              <li className='flex-container'>
+                {" "}
+                <span>
+                  <HotelIcon
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "30px",
+                      color: "#474747",
+                    }}
+                  />
+                </span>
+                {beds} Bed(s)
+              </li>
+              <li className='flex-container'>
+                {" "}
+                <span>
+                  <BathtubIcon
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "30px",
+                      color: "#474747",
+                    }}
+                  />
+                </span>
+                {bathrooms} Bathroom(s)
+              </li>
+              <li className='flex-container'>
+                {" "}
+                <span>
+                  <StarIcon
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "30px",
+                      color: "#474747",
+                    }}
+                  />
+                </span>
+                {review_scores_rating} out of 100
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+
+      <div className='grid-1 card'>
+        <h2>Detailed Description</h2>
+        <p>{description}</p>
+      </div>
+      <div className='grid-1 card'>
+        <h2>Notes</h2>
+        <p>{notes}</p>
       </div>
     </div>
   );
