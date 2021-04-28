70a114bf0bf89c969a87380c6f131f4b69880ec1
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-04-28 14:36:42 -0400

    can now refresh profile page w/o destruction and mayhem

--- client/src/components/favorites/Favorites.js
+++ client/src/components/favorites/Favorites.js
@@ -1,13 +1,19 @@
 import React from "react";
 import FavoriteItem from "./FavoriteItem";
 
-const Favorites = ({ yee, deleteFav }) => {
+const Favorites = ({ yee, deleteFav, favorites }) => {
   return (
     <div>
       <div>
-        {yee.map((item, index) => (
-          <FavoriteItem key={index} item={item} deleteFav={deleteFav} />
-        ))}
+        {yee &&
+          yee.map((item, index) => (
+            <FavoriteItem
+              key={index}
+              item={item}
+              deleteFav={deleteFav}
+              favorites={favorites}
+            />
+          ))}
       </div>
     </div>
   );
